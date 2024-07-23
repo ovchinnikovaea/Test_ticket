c8fec734e312a0f5aa00b4aac75ff7a95ba20818
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-07-23 22:17:21 +0300

    created main file

---
+++ src/test/java/ru/stmlabs/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stmlabs.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
